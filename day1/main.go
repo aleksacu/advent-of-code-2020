0d1939bcc446e35c62d64c808f76e8a38c76c1a5
AleksaC <aleksacukovic1@gmail.com>
2020-12-01 23:06:11 +0100

    Update day1 solution

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"strconv"
@@ -20,28 +19,28 @@ func readInput(filePath string) string {
 	return string(data)
 }
 
-func partOne(entries []int) (int, error) {
+func partOne(entries []int) int {
 	for i, v := range entries {
 		for _, e := range entries[i+1:] {
 			if v+e == 2020 {
-				return v * e, nil
+				return v * e
 			}
 		}
 	}
-	return 0, errors.New("No entries sum up to 2020")
+	return -1
 }
 
-func partTwo(entries []int) (int, error) {
+func partTwo(entries []int) int {
 	for i, v := range entries {
 		for j, e := range entries[i+1:] {
 			for _, n := range entries[j+1:] {
 				if v+e+n == 2020 {
-					return v * e * n, nil
+					return v * e * n
 				}
 			}
 		}
 	}
-	return 0, errors.New("No triplet sums up to 2020")
+	return -1
 }
 
 func main() {
@@ -57,10 +56,17 @@ func main() {
 		entries[i] = entry
 	}
 
-	solution, err := partOne(entries)
-	check(err)
-	fmt.Println("Part one:", solution)
+	solution := partOne(entries)
+	if solution == -1 {
+		fmt.Println("No two entries sum up to 2020")
+	} else {
+		fmt.Println("Part one:", solution)
+	}
 
-	solution, err = partTwo(entries)
-	fmt.Println("Part two:", solution)
+	solution = partTwo(entries)
+	if solution == -1 {
+		fmt.Println("No three entries sum up to 2020")
+	} else {
+		fmt.Println("Part two:", solution)
+	}
 }
