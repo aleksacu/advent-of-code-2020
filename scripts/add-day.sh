3d6531956c6c01054876e4ff8faa4f556a11c405
AleksaC <aleksacukovic1@gmail.com>
2020-12-01 23:45:32 +0100

    Add add-day.sh script

---
+++ scripts/add-day.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Meant to be ran from the root of the repo
+
+set -eou pipefail
+
+mkdir "day$1"
+
+cd riir
+
+mkdir "day$1"
+
+cat <<EOF > "day$1/main.rs"
+fn main() {
+
+}
+EOF
+
+cat <<EOF >> Cargo.toml
+
+[[bin]]
+name = "day$1"
+path = "day$1/main.rs"
+EOF
